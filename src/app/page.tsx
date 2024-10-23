9bd984d44535806063bf8b002158ad588bd4405e
Intime-SA <invertimesa@gmail.com>
2024-10-23 20:58:52 -0300

    woki app white text correctioN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,7 @@ export default async function ProductsPage() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8 text-center text-white">
-        WOKI &gt; APP
-      </h1>
+      <h1 className="text-3xl font-bold mb-8 text-center">WOKI &gt; APP</h1>
 
       <ProductList initialProducts={products} />
     </div>
