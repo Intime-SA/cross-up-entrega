5abb493ff90240ade5aecd75c6edc36a3b19144c
Intime-SA <invertimesa@gmail.com>
2024-10-23 19:56:37 -0300

    correccion leve de test/jest ruteo

--- jest.config.ts
+++ jest.config.ts
@@ -2,18 +2,21 @@ import type { Config } from "jest";
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  // Proporciona la ruta de tu app de Next.js para cargar next.config.js y archivos .env
   dir: "./",
 });
 
-// Add any custom config to be passed to Jest
+// Configuración personalizada de Jest
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   preset: "ts-jest",
+  moduleNameMapper: {
+    // Configura los alias para resolver rutas como '@/'
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// Exportar la configuración
 export default createJestConfig(config);
--- src/__tests__/app/homePage.tsx
+++ src/__tests__/app/homePage.tsx
@@ -1,11 +1,35 @@
-import ProductsPage from "@/app/page"; // Adjust the path as needed
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import ProductsPage from "@/app/page";
+import { getProducts } from "@/data/api/products";
 
-it("renders home page", async () => {
-  render(<ProductsPage />); // Renderiza el componente Home
+// Mock de la función getProducts
+jest.mock("@/data/api/products", () => ({
+  getProducts: jest.fn(),
+}));
 
-  // Espera a que el texto "RAMA DEV" esté presente en el documento
-  await waitFor(() => {
-    expect(screen.getByText("RAMA DEV")).toBeInTheDocument();
+describe("ProductsPage", () => {
+  it("should render the h1 element with the correct text and classes", async () => {
+    // Castea getProducts como jest.Mock para que TypeScript lo reconozca
+    const mockGetProducts = getProducts as jest.Mock;
+
+    // Simula que getProducts devuelve una lista vacía
+    mockGetProducts.mockResolvedValueOnce([]);
+
+    // Renderiza la página
+    render(await ProductsPage());
+
+    // Verifica que el h1 se renderiza con el texto correcto
+    const headingElement = screen.getByRole("heading", {
+      name: /Product List/i,
+    });
+    expect(headingElement).toBeInTheDocument();
+
+    // Verifica las clases CSS del h1
+    expect(headingElement).toHaveClass(
+      "text-3xl",
+      "font-bold",
+      "mb-8",
+      "text-center"
+    );
   });
 });
