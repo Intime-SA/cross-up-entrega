28bd66e1b189914316c638560dd441c2f58f8952
Intime-SA <invertimesa@gmail.com>
2024-10-23 23:50:22 -0300

    cambios jest library version

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "jest";
-import nextJest from "next/jest.js";
+import nextJest from "next/jest";
 
 const createJestConfig = nextJest({
   // Proporciona la ruta de tu app de Next.js para cargar next.config.js y archivos .env
@@ -16,7 +16,14 @@ const config: Config = {
     // Configura los alias para resolver rutas como '@/'
     "^@/(.*)$": "<rootDir>/src/$1",
   },
+  // Añade estas líneas para manejar archivos de estilo y otros archivos estáticos
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  // Ignora los archivos de test en la carpeta node_modules
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
 };
 
-// Exportar la configuración
+// createJestConfig es exportado de esta manera para asegurar que next/jest pueda cargar la configuración de Next.js que es asíncrona
 export default createJestConfig(config);
--- jest.d.ts
+++ jest.d.ts
@@ -1,10 +1,21 @@
 import "@testing-library/jest-dom";
 
 declare global {
-  // Asegúrate de que `fetch` esté en el contexto global
+  // Extend the Window interface
   interface Window {
     fetch: jest.Mock;
   }
+
+  // Add common Jest globals
+  const describe: typeof import("@jest/globals").describe;
+  const it: typeof import("@jest/globals").it;
+  const test: typeof import("@jest/globals").test;
+  const expect: typeof import("@jest/globals").expect;
+  const beforeEach: typeof import("@jest/globals").beforeEach;
+  const afterEach: typeof import("@jest/globals").afterEach;
+  const beforeAll: typeof import("@jest/globals").beforeAll;
+  const afterAll: typeof import("@jest/globals").afterAll;
+  const jest: typeof import("@jest/globals").jest;
 }
 
 export {};
